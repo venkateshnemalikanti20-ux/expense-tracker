896c15e011b2f8b9fc6beeb60a249f7a8ab5b587
venkateshnemalikanti20-ux <venkateshnemalikanti20@gmail.com>
2026-06-23 08:49:13 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,49 @@
-let otp = 0
+let otp = 0;
+
 function generate(){
-	let user = document.getElementById("name").value
-	if(user === ""){
-		alert("please enter user name")
-		return
-	}
-	otp = Math.floor(1000+Math.random()*9000)
-	alert("Your otp is:"+otp)	
+
+    let user =
+    document.getElementById("name").value;
+
+    if(user === ""){
+        alert("Please enter username");
+        return;
+    }
+
+    otp =
+    Math.floor(
+        1000 + Math.random() * 9000
+    );
+
+    alert("Your OTP is: " + otp);
 }
+
 function login(){
-	let uotp = document.getElementById("otp").value
-	if(Number(uotp) === otp){
-		localStorage.setItem(
-		"loggedIn","true"
-		)
-		otp=0
-		window.location.href="expense1.html"
-	}
-	else{
-		alert("Invalid otp")
-	}
+
+    let user =
+    document.getElementById("name").value;
+
+    let uotp =
+    document.getElementById("otp").value;
+
+    if(Number(uotp) === otp){
+
+        localStorage.setItem(
+            "loggedIn",
+            "true"
+        );
+
+        localStorage.setItem(
+            "username",
+            user
+        );
+
+        otp = 0;
+
+        window.location.href =
+        "expense1.html";
+    }
+    else{
+        alert("Invalid OTP");
+    }
 }
